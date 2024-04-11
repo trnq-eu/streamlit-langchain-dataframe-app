ed6616587452c1c70b5fe00e7f7f509aa2c2deb9
trinchero-promemoria <stefano.trinchero@promemoriagroup.com>
2024-04-11 12:35:25 +0200

    fix import langchain

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import pandas as pd
 from langchain.chat_models import ChatOpenAI
-from langchain.agents import create_pandas_dataframe_agent
+from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
 from langchain.agents.agent_types import AgentType
+
+
+
 
 
 # Page title
